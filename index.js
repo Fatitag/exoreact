5e1ef13a35093d786a48b4f9ad910070689a64d5
Fatitag <112667448+Fatitag@users.noreply.github.com>
2023-03-10 00:27:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Email from './Email';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+import reportWebVitals from './reportWebVitals';
+
+let r=ReactDOM.createRoot(document.getElementById("root"));
+r.render(<div> <Email/></div>)
